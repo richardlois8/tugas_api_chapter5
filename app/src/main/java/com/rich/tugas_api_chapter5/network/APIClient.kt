0ce0c9fe0233a47720a9113d25b6e8d09e66d2ff
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-10-03 21:46:51 +0700

    Creating API Client and Interface

---
+++ app/src/main/java/com/rich/tugas_api_chapter5/network/APIClient.kt
@@ -0,0 +1,25 @@
+package com.rich.tugas_api_chapter5.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object APIClient {
+    const val BASE_URL = "https://6254434289f28cf72b5aed04.mockapi.io/"
+
+    private val logging : HttpLoggingInterceptor
+        get() = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val instance : APIInterface by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        retrofit.create(APIInterface::class.java)
+    }
+}
---
+++ app/src/main/java/com/rich/tugas_api_chapter5/network/APIInterface.kt
@@ -0,0 +1,7 @@
+package com.rich.tugas_api_chapter5.network
+
+import retrofit2.http.GET
+
+class APIInterface {
+
+}
