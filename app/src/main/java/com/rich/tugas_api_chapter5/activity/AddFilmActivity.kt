5aae73d7e55ea4b2a870c2ee00c0d3494a5f2f30
farelibrahim34 <farrelibrahim34@gmail.com>
2022-10-03 22:20:51 +0700

    update post film

--- app/src/main/java/com/rich/tugas_api_chapter5/activity/AddFilmActivity.kt
+++ app/src/main/java/com/rich/tugas_api_chapter5/activity/AddFilmActivity.kt
@@ -2,11 +2,30 @@ package com.rich.tugas_api_chapter5.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.rich.tugas_api_chapter5.R
+import com.rich.tugas_api_chapter5.databinding.ActivityAddFilmBinding
+import com.rich.tugas_api_chapter5.viewmodel.ViewModelFilm
 
 class AddFilmActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAddFilmBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_film)
+        binding = ActivityAddFilmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+    fun addFilm(name : String, image : String, director : String, desc : String){
+        var viewModel = ViewModelProvider(this).get(ViewModelFilm::class.java)
+        viewModel.callPostApiFilm(name,image,director,desc)
+        viewModel.postLiveDataCar().observe(this,{
+            if(it != null){
+                Toast.makeText(this,"add data sukses", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
