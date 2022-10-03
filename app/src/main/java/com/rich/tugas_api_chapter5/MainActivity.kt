ff269640323b9d640da8385c8b84cc4156511b2d
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-10-03 21:16:49 +0700

    Initial commit

---
+++ app/src/main/java/com/rich/tugas_api_chapter5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rich.tugas_api_chapter5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
