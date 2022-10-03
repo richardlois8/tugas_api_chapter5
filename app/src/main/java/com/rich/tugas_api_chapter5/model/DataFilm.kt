b820292e409f009f20dc43f8498022a18099b394
Richard Lois <richard.lois@ti.ukdw.ac.id>
2022-10-03 23:23:04 +0700

    Fixing data film property name

--- app/src/main/java/com/rich/tugas_api_chapter5/model/DataFilm.kt
+++ app/src/main/java/com/rich/tugas_api_chapter5/model/DataFilm.kt
@@ -1,9 +1,9 @@
 package com.rich.tugas_api_chapter5.model
 
 data class DataFilm(
-    val nama : String,
-    val imag : String,
+    val name : String,
+    val image : String,
     val director : String,
-    val desc : String
+    val description : String
 
 )
