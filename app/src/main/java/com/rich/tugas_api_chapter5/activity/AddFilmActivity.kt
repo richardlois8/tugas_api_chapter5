ed401080851de4307a7de5899a61ba246eca2d3d
farelibrahim34 <farrelibrahim34@gmail.com>
2022-10-03 21:33:36 +0700

    add item film

---
+++ app/src/main/java/com/rich/tugas_api_chapter5/activity/AddFilmActivity.kt
@@ -0,0 +1,12 @@
+package com.rich.tugas_api_chapter5.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rich.tugas_api_chapter5.R
+
+class AddFilmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_film)
+    }
+}
