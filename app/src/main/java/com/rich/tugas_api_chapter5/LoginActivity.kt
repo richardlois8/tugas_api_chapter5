5a3b29114b4d06371c8dd51b80195634f7c61bae
Syifaseptiana12 <syifaseptiana.di13@gmail.com>
2022-10-05 07:09:03 +0700

    login activity

--- app/src/main/java/com/rich/tugas_api_chapter5/LoginActivity.kt
+++ app/src/main/java/com/rich/tugas_api_chapter5/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.rich.tugas_api_chapter5
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
@@ -9,8 +11,9 @@ import com.rich.tugas_api_chapter5.databinding.ActivityRegisterBinding
 import com.rich.tugas_api_chapter5.viewmodel.ViewModelUser
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLoginBinding
-    private lateinit var userVM : ViewModelUser
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var userVM: ViewModelUser
+    private lateinit var sharedprefences :SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +21,18 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         userVM = ViewModelProvider(this).get(ViewModelUser::class.java)
-
         binding.tvRegister.setOnClickListener {
+            var inUser = binding.etUsername.text.toString()
+            var inPassword = binding.etPassword.text.toString()
+
+
+
             gotoRegister()
+        sharedprefences = this.getSharedPreferences("user", Context.MODE_PRIVATE)
+
         }
+
+
     }
 
     private fun gotoRegister() {
